8e52d211994f669220d74dc6db5926470cdd8fd0
prospero <g0lden_vo1d@protonmail.com>
2023-10-04 18:08:00 +0200

    testing

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Users</title>
+    <meta name="description" content="Description">
+    <meta name="author" content="Author">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+
+    <style>
+        #container {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="container">
+        hi
+    </div>
+</body>
+
+</html>
